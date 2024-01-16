90c0f9f3ea0ccf7f89e09dc1be5c8c193d94112e
闫润宁 <yanrunning@goyoo.com>
2024-01-16 14:57:05 +0800

    fix: ~

--- server.js
+++ server.js
@@ -1,96 +1,33 @@
-// Virtual entry point for the app
-import * as remixBuild from '@remix-run/dev/server-build';
-import {
-  createRequestHandler,
-  getStorefrontHeaders,
-} from '@shopify/remix-oxygen';
-import {
-  cartGetIdDefault,
-  cartSetIdDefault,
-  createCartHandler,
-  createStorefrontClient,
-  storefrontRedirect,
-} from '@shopify/hydrogen';
+import {createStorefrontClient} from '@shopify/hydrogen';
+import {createRequestHandler} from '@vercel/remix';
 
-import {HydrogenSession} from '~/lib/session.server';
-import {getLocaleFromRequest} from '~/lib/utils';
-
-/**
- * Export a fetch handler in module format.
- */
 export default {
-  /**
-   * @param {Request} request
-   * @param {Env} env
-   * @param {ExecutionContext} executionContext
-   */
   async fetch(request, env, executionContext) {
-    try {
-      /**
-       * Open a cache instance in the worker and a custom session instance.
-       */
-      if (!env?.SESSION_SECRET) {
-        throw new Error('SESSION_SECRET environment variable is not set');
-      }
-
-      const waitUntil = executionContext.waitUntil.bind(executionContext);
-      const [cache, session] = await Promise.all([
-        caches.open('hydrogen'),
-        HydrogenSession.init(request, [env.SESSION_SECRET]),
-      ]);
-
-      /**
-       * Create Hydrogen's Storefront client.
-       */
-      const {storefront} = createStorefrontClient({
-        cache,
-        waitUntil,
-        i18n: getLocaleFromRequest(request),
-        publicStorefrontToken: env.PUBLIC_STOREFRONT_API_TOKEN,
-        privateStorefrontToken: env.PRIVATE_STOREFRONT_API_TOKEN,
-        storeDomain: env.PUBLIC_STORE_DOMAIN,
-        storefrontId: env.PUBLIC_STOREFRONT_ID,
-        storefrontHeaders: getStorefrontHeaders(request),
-      });
-
-      const cart = createCartHandler({
-        storefront,
-        getCartId: cartGetIdDefault(request.headers),
-        setCartId: cartSetIdDefault(),
-      });
-
-      /**
-       * Create a Remix request handler and pass
-       * Hydrogen's Storefront client to the loader context.
-       */
-      const handleRequest = createRequestHandler({
-        build: remixBuild,
-        mode: process.env.NODE_ENV,
-        getLoadContext: () => ({
-          session,
-          waitUntil,
-          storefront,
-          cart,
-          env,
-        }),
-      });
-
-      const response = await handleRequest(request);
-
-      if (response.status === 404) {
-        /**
-         * Check for redirects only when there's a 404 from the app.
-         * If the redirect doesn't exist, then `storefrontRedirect`
-         * will pass through the 404 response.
-         */
-        return storefrontRedirect({request, response, storefront});
-      }
-
-      return response;
-    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.error(error);
-      return new Response('An unexpected error occurred', {status: 500});
-    }
+    const {storefront} = createStorefrontClient({
+      // Required: Storefront API credentials
+      privateStorefrontToken: env.PRIVATE_STOREFRONT_API_TOKEN,
+      publicStorefrontToken: env.PUBLIC_STOREFRONT_API_TOKEN,
+      storefrontApiVersion: env.PUBLIC_STOREFRONT_API_VERSION,
+      storeDomain: `https://${env.PUBLIC_STORE_DOMAIN}`,
+      storefrontHeaders: {
+        // Pass a buyerIp to prevent being flagged as a bot
+        buyerIp: 'customer_IP_address', // Platform-specific method to get request IP
+        cookie: request.headers.get('cookie'),  // Required for Shopify Analytics
+        purpose: request.headers.get('purpose'), // Used for debugging purposes
+      },
+      i18n: {
+        country: 'country_code',
+        language: 'language_code',
+      },
+      cache: () => {},
+      waitUntil: () => {},
+      // Additional platform-specific configuration...
+    });
+    const handleRequest = createRequestHandler({
+      // Inject the Storefront API client into the Remix context
+      getLoadContext: () => ({storefront}),
+      // Additional platform-specific configuration...
+    });
+    return handleRequest(request);
   },
 };
